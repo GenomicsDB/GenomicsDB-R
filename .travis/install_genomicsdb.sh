d39fab61a33e8261d5a21eed0218e182c6ecb560
Nalini Ganapati <35076948+nalinigans@users.noreply.github.com>
2019-06-18 20:02:23 -0700

    Update install_genomicsdb.sh

--- .travis/install_genomicsdb.sh
+++ .travis/install_genomicsdb.sh
@@ -22,7 +22,7 @@ install_prerequisites() {
 }
 
 install_genomicsdb() {
-	git clone https://github.com/GenomicsDB/GenomicsDB -b nalini_macos $GENOMICSDB_DIR &&
+	git clone https://github.com/GenomicsDB/GenomicsDB -b nalini_api $GENOMICSDB_DIR &&
 			pushd $GENOMICSDB_DIR && git submodule update --recursive --init && popd &&
 			mkdir $GENOMICSDB_BUILD_DIR &&
 			pushd $GENOMICSDB_BUILD_DIR &&
