8d3d5da255461abc72354d34f2702e30be06239a
nalinigans <nalini@omicsautomation.com>
2020-03-02 11:12:08 -0800

    More travis/build changes to support local packaging

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-rm -f  src/Makevars src/*.o src/*.so config.log config.status
-rm -fr genomicsdb.Rcheck
+./cleanup
 
-./configure --with-genomicsdb=${HOME}
+GENOMICSDB_HOME=$HOME
 
 # R -e 'library(Rcpp); compileAttributes(".")'
 R CMD build .
-R CMD install --preclean --configure-args="--with-genomicsdb=${HOME}" genomicsdb_0.0.2.tar.gz
-R CMD check --no-manual genomicsdb_0.0.2.tar.gz
+R CMD INSTALL --preclean --configure-args="--with-genomicsdb=${GENOMICSDB_HOME}" genomicsdb_0.0.2.tar.gz
+R CMD check --no-manual --install-args="--configure-args='--with-genomicsdb=${GENOMICSDB_HOME}'" genomicsdb_0.0.2.tar.gz
 R -e 'library(devtools); test()'
