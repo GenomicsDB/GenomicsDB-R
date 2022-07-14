004b00688fd0db42ddb82d6b30c0d827d2a386a7
nalinigans <nalini@omicsautomation.com>
2022-07-14 10:39:54 -0700

    Use GitHub Actions for CI

--- .github/scripts/install_genomicsdb.sh
+++ .github/scripts/install_genomicsdb.sh
@@ -13,7 +13,6 @@ install_genomicsdb() {
   if  [[ $(uname) == "Darwin" ]]; then export MACOSX_DEPLOYMENT_TARGET=10.13; fi &&
 	git clone https://github.com/GenomicsDB/GenomicsDB --recursive -b $GENOMICSDB_BRANCH $GENOMICSDB_DIR &&
       INSTALL_PREFIX=$HOME/genomicsdb_prereqs $GENOMICSDB_DIR/scripts/prereqs/install_prereqs.sh &&
-      if [[ $(uname) == "Darwin" ]]; then brew install open-mpi; fi &&
       source $HOME/genomicsdb_prereqs.sh &&
 			mkdir $GENOMICSDB_BUILD_DIR &&
 			pushd $GENOMICSDB_BUILD_DIR &&
