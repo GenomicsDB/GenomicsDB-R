d97aa8b64f6f3cf548fdfd026759e62e7d7fe5a6
nalinigans <nalini@omicsautomation.com>
2022-07-14 12:49:13 -0700

    Still trying

--- .github/scripts/install_genomicsdb.sh
+++ .github/scripts/install_genomicsdb.sh
@@ -9,7 +9,7 @@ CMAKE=cmake
 BUILD_FOR_PYTHON=true
 
 install_genomicsdb() {
-  echo "Starting install of GenomicsDB"
+  echo "+++ Starting install of GenomicsDB..."
   if  [[ $(uname) == "Darwin" ]]; then export MACOSX_DEPLOYMENT_TARGET=10.13; export SUDO="" ; else export SUDO="sudo"; fi &&
 	git clone https://github.com/GenomicsDB/GenomicsDB --recursive -b $GENOMICSDB_BRANCH $GENOMICSDB_DIR &&
       $GENOMICSDB_DIR/scripts/prereqs/install_prereqs.sh &&
@@ -22,11 +22,13 @@ install_genomicsdb() {
          echo "  $CMAKE .. -DCMAKE_INSTALL_PREFIX=$GENOMICSDB_INSTALL_DIR -DBUILD_DISTRIBUTABLE_LIBRARY=1 -DBUILD_JAVA=0 -DUSE_HDFS=0 -DBUILD_FOR_PYTHON=$BUILD_FOR_PYTHON" &&
          $CMAKE .. -DCMAKE_INSTALL_PREFIX=$GENOMICSDB_INSTALL_DIR -DBUILD_DISTRIBUTABLE_LIBRARY=1 -DBUILD_JAVA=0 -DUSE_HDFS=0 -DBUILD_FOR_PYTHON=$BUILD_FOR_PYTHON && make
       fi
-			make -j 4 &&
+			make &&
 			make install &&
 			test -f $GENOMICSDB_INSTALL_DIR/include/genomicsdb.h &&
 			test -f $GENOMICSDB_INSTALL_DIR/lib/libtiledbgenomicsdb.so &&
-			popd
+			popd &&
+      echo "+++ GenomicsDB Install DONE" || echo "*** Failed to Install GenomicsDB"
+      
 }
 
 install_genomicsdb &&
